f8e488bb3370c0d2d306e81994c7b6c12aa7181a
wkwksama <mailpakgasing@gmail.com>
2019-12-24 22:05:51 +0700

    index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,72 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
+  <v-container>
+    <v-row class="mt-1">
+      <v-col v-for="i in 6"
+             :key="i"
+             cols="4"
+             class="text-center my-0 py-0"
+      >
+        <v-btn :style="{ borderWidth: '2px', borderRadius: '25%' }"
+               icon
+               dark
+               large
+               fab
+        >
+          <v-icon v-text="'mdi-home'" large />
+        </v-btn>
+        <p>{{ 'home' }}</p>
+      </v-col>
+    </v-row>
+
+    <v-card v-for="i in 6"
+            :key="i"
+            class="mt-2"
+            max-width="400"
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
+      <div class="body-1 px-4 py-2">
+        ahmad sukri
       </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
+      <v-img class="white--text align-end"
+             height="200px"
+             src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-row class="px-4">
+          <v-col class="title">
+            asdfsdf
+          </v-col>
+          <v-col cols="auto">
+            <v-chip color="white"
+                    text-color="error"
+                    small
             >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+              belum disakljsd
+              <v-icon right
+                      color="error"
+                      small
+              >
+                mdi-account
+              </v-icon>
+            </v-chip>
+          </v-col>
+        </v-row>
+      </v-img>
+
+      <v-card-text class="text--primary">
+        <div>
+          <v-icon>
+            mdi-arrow-up-bold
+          </v-icon>
+          16
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
   }
 }
 </script>

574211f817a38c6296ff5fe60d3c7ba2c63dbb35
wkwksama <mailpakgasing@gmail.com>
2020-01-05 11:25:33 +0700

    locaiton save to localstorage

--- constants/storageKeys.js
+++ constants/storageKeys.js
@@ -1,5 +1,8 @@
 const storagekeys = {
-  AUTH_TOKEN: 'auth_token'
+  AUTH_TOKEN: 'auth_token',
+  LOCATION_LAT: 'location_lat',
+  LOCATION_LNG: 'location_lng',
+  LOCATION_UPDATED_AT: 'location_updated_at'
 }
 
 export { storagekeys as default }
--- plugins/location.client.js
+++ plugins/location.client.js
@@ -1,5 +1,10 @@
+import storageKeys from '../constants/storageKeys'
+
 export default () => {
   navigator.geolocation.getCurrentPosition((position) => {
     console.log('location', position)
+    localStorage.setItem(storageKeys.LOCATION_LAT, position.coords.latitude)
+    localStorage.setItem(storageKeys.LOCATION_LNG, position.coords.longitude)
+    localStorage.setItem(storageKeys.LOCATION_UPDATED_AT, position.timestamp)
   })
 }

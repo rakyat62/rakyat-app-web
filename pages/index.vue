8b843f6878a11df72fe9e08010d0313b465188ed
wkwksama <mailpakgasing@gmail.com>
2020-01-11 11:56:09 +0700

    fix index: screen width md

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,87 @@
 <template>
   <v-container>
-    <v-row v-if="loadingIncidentLabels" class="mt-1">
-      <v-col v-for="i in 3"
-             :key="`loadinglabel${i}`"
-             cols="4"
-             class="py-5"
-      >
-        <v-skeleton-loader type="avatar" tile />
-      </v-col>
-    </v-row>
-    <v-row v-else class="mt-1">
-      <v-col v-for="label in incidentLabels"
-             :key="label.id"
-             cols="4"
-             class="text-center my-0 py-0"
-      >
-        <v-btn :style="{ borderWidth: '2px', borderRadius: '25%' }"
-               :to="`/new/incident/${label.id}`"
-               icon
-               large
-               fab
-        >
-          <v-icon v-text="label.icon" large />
-        </v-btn>
-        <p>{{ label.name }}</p>
-      </v-col>
-    </v-row>
+    <v-row justify="center">
+      <v-col md="4">
+        <v-row v-if="loadingIncidentLabels" class="mt-1">
+          <v-col v-for="i in 3"
+                 :key="`loadinglabel${i}`"
+                 cols="4"
+                 class="py-5"
+          >
+            <v-skeleton-loader type="avatar" tile />
+          </v-col>
+        </v-row>
+        <v-row v-else class="mt-1">
+          <v-col v-for="label in incidentLabels"
+                 :key="label.id"
+                 cols="4"
+                 class="text-center my-0 py-0"
+          >
+            <v-btn :style="{ borderWidth: '2px', borderRadius: '25%' }"
+                   :to="`/new/incident/${label.id}`"
+                   icon
+                   large
+                   fab
+            >
+              <v-icon v-text="label.icon" large />
+            </v-btn>
+            <p>{{ label.name }}</p>
+          </v-col>
+        </v-row>
 
-    <template v-if="loadingIncidents">
-      <v-skeleton-loader v-for="i in 4"
-                         :key="`loadinginc${i}`"
-                         type="card-heading, card"
-                         class="mt-4"
-      />
-    </template>
-    <template v-else>
-      <v-card v-for="incident in incidents"
-              :key="incident.id"
-              class="mt-4"
-      >
-        <div class="body-1 px-4 py-2">
-          {{ incident.createdBy.username }}
-        </div>
-        <v-img class="white--text align-end"
-               height="200px"
-               src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-        >
-          <v-row class="px-4">
-            <v-col class="title">
-              {{ incident.label.name }}
-            </v-col>
-            <v-col cols="auto">
-              <v-chip color="white"
-                      text-color="error"
-                      small
-              >
-                belum disakljsd
-                <v-icon right
-                        color="error"
-                        small
-                >
-                  mdi-account
+        <template v-if="loadingIncidents">
+          <v-skeleton-loader v-for="i in 4"
+                             :key="`loadinginc${i}`"
+                             type="card-heading, card"
+                             class="mt-4"
+          />
+        </template>
+        <template v-else>
+          <v-card v-for="incident in incidents"
+                  :key="incident.id"
+                  class="mt-4"
+          >
+            <div class="body-1 px-4 py-2">
+              {{ incident.createdBy.username }}
+            </div>
+            <v-img class="white--text align-end"
+                   height="200px"
+                   src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-row class="px-4">
+                <v-col class="title">
+                  {{ incident.label.name }}
+                </v-col>
+                <v-col cols="auto">
+                  <v-chip color="white"
+                          text-color="error"
+                          small
+                  >
+                    belum disakljsd
+                    <v-icon right
+                            color="error"
+                            small
+                    >
+                      mdi-account
+                    </v-icon>
+                  </v-chip>
+                </v-col>
+              </v-row>
+            </v-img>
+
+            <v-card-text class="text--primary">
+              <div class="caption">
+                <!-- <v-icon>
+                  mdi-arrow-up-bold
                 </v-icon>
-              </v-chip>
-            </v-col>
-          </v-row>
-        </v-img>
-
-        <v-card-text class="text--primary">
-          <div class="caption">
-            <!-- <v-icon>
-              mdi-arrow-up-bold
-            </v-icon>
-            16 -->
-            {{ formatDate(incident.createdAt, 'relative') }}
-          </div>
-        </v-card-text>
-      </v-card>
-    </template>
+                16 -->
+                {{ formatDate(incident.createdAt, 'relative') }}
+              </div>
+            </v-card-text>
+          </v-card>
+        </template>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 

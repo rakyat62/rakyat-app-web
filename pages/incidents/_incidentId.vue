c4572e607db8962c53f8b535f6220cc520aa162c
wkwksama <mailpakgasing@gmail.com>
2020-01-14 20:44:58 +0700

    incident details loading

--- pages/incidents/_incidentId.vue
+++ pages/incidents/_incidentId.vue
@@ -1,113 +1,132 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col class="display-1">
-        {{ incident.label ? incident.label.name : '-' }} <span class="grey--text">{{ `#${incident.id}` }}</span>
-      </v-col>
-      <v-col cols="auto">
-        <v-btn :style="{textTransform:'capitalize'}"
-               color="primary"
-        >
-          Update Riwayat
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-chip :text-color="`${$vuetify.theme.dark ? 'white' : 'black'}`"
-            :color="chipStatus.color"
-            outlined
-            label
-            class="white"
-            small
-    >
-      {{ chipStatus.text }}
-      <v-icon v-text="chipStatus.icon"
-              :color="chipStatus.color"
-              right
-              small
+    <template v-if="loadingIncident">
+      <v-row>
+        <v-col>
+          <v-skeleton-loader type="heading" />
+        </v-col>
+        <v-col cols="auto">
+          <v-skeleton-loader type="button" />
+        </v-col>
+      </v-row>
+      <v-skeleton-loader type="chip" />
+      <v-skeleton-loader type="sentences" class="mt-4" />
+      <v-skeleton-loader v-for="i in 4"
+                         :key="`loadingcind${i}`"
+                         type="list-item-avatar-three-line"
+                         class="mt-5"
       />
-    </v-chip>
-    <p v-text="incident.information" />
-    <v-row>
-      <v-col v-for="i in 6"
-             :key="`img_incident_${i}`"
-             cols="4"
-             md="2"
-             lg="2"
-             class="pa-1"
+    </template>
+    <template v-else>
+      <v-row>
+        <v-col class="display-1">
+          {{ incident.label ? incident.label.name : '-' }} <span class="grey--text">{{ `#${incident.id}` }}</span>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn :style="{textTransform:'capitalize'}"
+                 color="primary"
+          >
+            Update Riwayat
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-chip :text-color="`${$vuetify.theme.dark ? 'white' : 'black'}`"
+              :color="chipStatus.color"
+              outlined
+              label
+              class="white"
+              small
       >
-        <v-img :class="`grey ${ $vuetify.theme.dark ? 'darken' : 'lighten'}-3`"
-               class="align-end"
-               contain
-               height="100px"
-               src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        {{ chipStatus.text }}
+        <v-icon v-text="chipStatus.icon"
+                :color="chipStatus.color"
+                right
+                small
         />
-      </v-col>
-    </v-row>
-
-    <v-timeline align-top dense>
-      <template v-for="(history, index) in incidentHistories">
-        <v-timeline-item v-if="history.type === 'COMMENT'"
-                         :key="`tl_${index}`"
-                         large
+      </v-chip>
+      <p v-text="incident.information" />
+      <v-row>
+        <v-col v-for="i in 6"
+               :key="`img_incident_${i}`"
+               cols="4"
+               md="2"
+               lg="2"
+               class="pa-1"
         >
-          <template v-slot:icon>
-            <v-avatar>
-              <img :src="avaUrlUser">
-            </v-avatar>
-          </template>
-          <template v-slot:opposite>
-            <span>Tus eu perfecto</span>
-          </template>
-          <v-card class="elevation-2">
+          <v-img :class="`grey ${ $vuetify.theme.dark ? 'darken' : 'lighten'}-3`"
+                 class="align-end"
+                 contain
+                 height="100px"
+                 src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          />
+        </v-col>
+      </v-row>
+
+      <v-timeline align-top dense>
+        <template v-for="(history, index) in incidentHistories">
+          <v-timeline-item v-if="history.type === 'COMMENT'"
+                           :key="`tl_${index}`"
+                           large
+          >
+            <template v-slot:icon>
+              <v-avatar>
+                <img :src="avaUrlUser">
+              </v-avatar>
+            </template>
+            <template v-slot:opposite>
+              <span>Tus eu perfecto</span>
+            </template>
+            <v-card class="elevation-2">
+              <v-card-title class="py-2">
+                <span v-text="history.createdBy.username" class="title" />
+                <span v-text="formatDate(history.createdAt, 'relative')" class="body-2 ml-4" />
+              </v-card-title>
+              <v-card-text v-text="history.content" />
+            </v-card>
+          </v-timeline-item>
+
+          <v-card v-if="history.type === 'FOLLOW_UP'"
+                  :key="`tl_${index}`"
+                  class="elevation-2 mb-6"
+          >
             <v-card-title class="py-2">
-              <span v-text="history.createdBy.username" class="title" />
+              <span class="title">Update dari {{ history.createdBy.username }}</span>
               <span v-text="formatDate(history.createdAt, 'relative')" class="body-2 ml-4" />
             </v-card-title>
-            <v-card-text v-text="history.content" />
+            <v-card-text>
+              <p v-text="history.content" class="mt-2" />
+              <v-row>
+                <v-col v-for="i in 6"
+                       :key="`img_incident_${i}`"
+                       cols="4"
+                       md="2"
+                       lg="2"
+                       class="pa-1"
+                >
+                  <v-img class="grey lighten-4 align-end"
+                         contain
+                         height="120px"
+                         src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                  />
+                </v-col>
+              </v-row>
+            </v-card-text>
           </v-card>
-        </v-timeline-item>
+        </template>
+      </v-timeline>
+      <v-divider />
 
-        <v-card v-if="history.type === 'FOLLOW_UP'"
-                :key="`tl_${index}`"
-                class="elevation-2 mb-6"
-        >
-          <v-card-title class="py-2">
-            <span class="title">Update dari {{ history.createdBy.username }}</span>
-            <span v-text="formatDate(history.createdAt, 'relative')" class="body-2 ml-4" />
-          </v-card-title>
-          <v-card-text>
-            <p v-text="history.content" class="mt-2" />
-            <v-row>
-              <v-col v-for="i in 6"
-                     :key="`img_incident_${i}`"
-                     cols="4"
-                     md="2"
-                     lg="2"
-                     class="pa-1"
-              >
-                <v-img class="grey lighten-4 align-end"
-                       contain
-                       height="120px"
-                       src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                />
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </template>
-    </v-timeline>
-    <v-divider />
-
-    <v-card class="mt-6 pa-4" color="white">
-      <v-textarea outlined
-                  hide-details
-                  label="Tulis Komentar"
-      />
-      <v-btn v-text="'Kirim'"
-             color="primary"
-             class="mt-2"
-      />
-    </v-card>
+      <v-card class="mt-6 pa-4" color="white">
+        <v-textarea outlined
+                    hide-details
+                    label="Tulis Komentar"
+        />
+        <v-btn v-text="'Kirim'"
+               color="primary"
+               class="mt-2"
+        />
+      </v-card>
+    </template>
   </v-container>
 </template>
 
